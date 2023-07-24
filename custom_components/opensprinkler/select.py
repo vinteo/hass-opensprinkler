04b7c5f0b97eda47b2b1edad071eca487dcffd98
Ed Leckert <Ed@EdLeckertImages.com>
2023-07-24 08:09:13 -0600

    Add remaining selects

--- custom_components/opensprinkler/select.py
+++ custom_components/opensprinkler/select.py
@@ -31,18 +31,20 @@ def _create_entities(hass: HomeAssistant, entry: dict):
     name = entry.data[CONF_NAME]
 
     for _, program in controller.programs.items():
-        entities.append(ProgramRestrictionSelect(entry, name, program, coordinator))
+        entities.append(ProgramRestrictionsSelect(entry, name, program, coordinator))
+        entities.append(ProgramTypeSelect(entry, name, program, coordinator))
+        entities.append(ProgramStartTimesSelect(entry, name, program, coordinator))
 
     return entities
 
 
-class ProgramRestrictionSelect(
+class ProgramRestrictionsSelect(
     OpenSprinklerProgramEntity, OpenSprinklerSelect, SelectEntity
 ):
-    """Represent select for the odd/even restriction of a program."""
+    """Represent select for the odd/even restrictions of a program."""
 
     def __init__(self, entry, name, program, coordinator):
-        """Set up a new OpenSprinkler program select."""
+        """Set up a new OpenSprinkler program select for restrictions."""
         self._program = program
         self._entity_type = "select"
         super().__init__(entry, name, coordinator)
@@ -90,4 +92,108 @@ class ProgramRestrictionSelect(
             case "Even Days Only":
                 value = 2
         await self._program.set_odd_even_restriction(value)
+        await self._coordinator.async_request_refresh()
+
+
+class ProgramTypeSelect(OpenSprinklerProgramEntity, OpenSprinklerSelect, SelectEntity):
+    """Represent select for the type of a program."""
+
+    def __init__(self, entry, name, program, coordinator):
+        """Set up a new OpenSprinkler program select for program type."""
+        self._program = program
+        self._entity_type = "select"
+        super().__init__(entry, name, coordinator)
+
+    @property
+    def name(self) -> str:
+        """Return the name of this select."""
+        return f"{self._program.name} Type"
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique, Home Assistant friendly identifier for this entity."""
+        return slugify(
+            f"{self._entry.unique_id}_{self._entity_type}_type_{self._program.index}"
+        )
+
+    @property
+    def icon(self) -> str:
+        """Return icon."""
+        return "mdi:calendar-range"
+
+    @property
+    def options(self) -> list[str]:
+        """A list of available options as strings"""
+        return ["Weekday", "Interval"]
+
+    @property
+    def current_option(self) -> str:
+        """The current select option"""
+        match self._program.program_schedule_type:
+            case 0:
+                return "Weekday"
+            case 3:
+                return "Interval"
+
+    async def async_select_option(self, option: str) -> None:
+        """Change the selected option."""
+        match option:
+            case "Weekday":
+                value = 0
+            case "Interval":
+                value = 3
+        await self._program.set_program_schedule_type(value)
+        await self._coordinator.async_request_refresh()
+
+
+class ProgramStartTimesSelect(
+    OpenSprinklerProgramEntity, OpenSprinklerSelect, SelectEntity
+):
+    """Represent select for additional start times of a program."""
+
+    def __init__(self, entry, name, program, coordinator):
+        """Set up a new OpenSprinkler program select for start times."""
+        self._program = program
+        self._entity_type = "select"
+        super().__init__(entry, name, coordinator)
+
+    @property
+    def name(self) -> str:
+        """Return the name of this select."""
+        return f"{self._program.name} Additional Start Times"
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique, Home Assistant friendly identifier for this entity."""
+        return slugify(
+            f"{self._entry.unique_id}_{self._entity_type}_start_times_{self._program.index}"
+        )
+
+    @property
+    def icon(self) -> str:
+        """Return icon."""
+        return "mdi:clock-start"
+
+    @property
+    def options(self) -> list[str]:
+        """A list of available options as strings"""
+        return ["Repeating", "Fixed"]
+
+    @property
+    def current_option(self) -> str:
+        """The current select option"""
+        match self._program.start_time_type:
+            case 0:
+                return "Repeating"
+            case 1:
+                return "Fixed"
+
+    async def async_select_option(self, option: str) -> None:
+        """Change the selected option."""
+        match option:
+            case "Repeating":
+                value = 0
+            case "Fixed":
+                value = 1
+        await self._program.set_start_time_type(value)
         await self._coordinator.async_request_refresh()
